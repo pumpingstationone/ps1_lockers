903a314a839e58cc52cd4a49b02bb9adad7306f6
Andrew Wingate <1464801+anfroholic@users.noreply.github.com>
2024-09-01 04:29:10 -0500

    update actual locker status

--- app/locker_helper.py
+++ app/locker_helper.py
@@ -4,11 +4,41 @@ lockers = {
     'neverland/lockers': [],
     'neverland/floor': [],
     'pallet_racks': [
-    [{"name":"","address":1,"status":"empty"},{"name":"","address":6,"status":"empty"},{"name":"Harry Spyceddragon","address":11,"status":"owned"},{"name":"","address":16,"status":"empty"},{"name":"","address":21,"status":"empty"},{"name":"","address":26,"status":"empty"}],
-    [{"name":"","address":2,"status":"empty"},{"name":"","address":7,"status":"empty"},{"name":"test2","address":12,"status":"full","date":"2024-08-25T05:08:45Z"},{"name":"","address":17,"status":"empty"},{"name":"test again","address":22,"status":"owned"},{"name":"","address":27,"status":"empty"}],
-    [{"name":"","address":3,"status":"empty"},{"name":"","address":8,"status":"empty"},{"name":"","address":13,"status":"empty"},{"name":"Dan Ennion","address":18,"status":"owned"},{"name":"","address":23,"status":"empty"},{"name":"","address":28,"status":"empty"}],
-    [{"name":"","address":4,"status":"empty"},{"name":"","address":9,"status":"empty"},{"name":"","address":14,"status":"empty"},{"name":"","address":19,"status":"empty"},{"name":"James Lamken","address":24,"status":"owned"},{"name":"test1","address":29,"status":"full","date":"2024-08-15T06:08:45.614Z"}],
-    [{"name":"","address":5,"status":"empty"},{"name":"","address":10,"status":"empty"}, {"name":"","address":15,"status":"empty"},{"name":"","address":20,"status":"empty"},{"name":"Harry Spyceddragon","address":25,"status":"owned"},{"name":"test2","address":30,"status":"full","date":"2024-08-21T05:20:45.614Z"}]
+    [{"name":"Eric Beauchamp","address":1,"status":"owned"},
+     {"name":"George Hoffman","address":6,"status":"owned"},
+     {"name":"","address":11,"status":"empty"},
+     {"name":"","address":16,"status":"empty"},
+     {"name":"","address":21,"status":"empty"},
+     {"name":"Andrew Eubanks ","address":26,"status":"owned"}],
+    
+    [{"name":"","address":2,"status":"empty"},
+     {"name":"Andrew E.H.","address":7,"status":"owned"},
+     {"name":"Chris Hartman","address":12,"status":"owned"},
+     {"name":"","address":17,"status":"empty"},
+     {"name":"","address":22,"status":"empty"},
+     {"name":"","address":27,"status":"empty"}],
+    
+    [{"name":"Maria Savrasova","address":3,"status":"owned"},
+     {"name":"Abel Mebratu","address":8,"status":"owned"},
+     {"name":"","address":13,"status":"empty"},
+     {"name":"Brian Desmond","address":18,"status":"owned"},
+     {"name":"Anthony Lighthall","address":23,"status":"owned"},
+     {"name":"Atticus Lazenby ","address":28,"status":"owned"}],
+    
+    [{"name":"","address":4,"status":"empty"},
+     {"name":"","address":9,"status":"empty"},
+     {"name":"","address":14,"status":"empty"},
+     {"name":"Mark Nielsen","address":19,"status":"owned"},
+     {"name":"","address":24,"status":"empty"},
+     # {"name":"","address":29,"status":"empty"}],
+     {"name":"test1","address":29,"status":"full","date":"2024-09-15T06:08:45.614Z"}],
+    
+    [{"name":"","address":5,"status":"empty"},
+     {"name":"","address":10,"status":"empty"}, 
+     {"name":"","address":15,"status":"empty"},
+     {"name":"","address":20,"status":"empty"},
+     {"name":"","address":25,"status":"empty"},
+     {"name":"Andrew Masek","address":30,"status":"owned"}]
 ],
 }
 
